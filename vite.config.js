2b6c418f3330e329dfd71862cdd78d1befc0991d
William Brewer <william@smallbusinessessupport.services>
2025-02-11 17:48:56 -0500

    show route in Omnibar

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    historyApiFallback: true
+  },
+  base: '/',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
