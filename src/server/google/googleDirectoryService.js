7492f8df3afd8f1f94c784715a137cf7807fe51e
William Brewer <william@smallbusinessessupport.services>
2025-02-21 22:04:47 -0500

    create user object

--- src/server/google/googleDirectoryService.js
+++ src/server/google/googleDirectoryService.js
@@ -7,8 +7,28 @@ class GoogleDirectoryService {
         customer: 'my_customer',
         maxResults: 10,
         orderBy: 'email',
+        projection: 'full'
       });
-      return res.data.users;
+
+      return res.data.users.map(user => ({ 
+        id: user.id,
+        name: user.name.fullName,
+        email: user.primaryEmail,
+        phone: user.phones?.[0]?.value,
+        role: user.organizations?.[0]?.title || 'Team Member',
+        department: user.organizations?.[0]?.department,
+        description: user.organizations?.[0]?.description,
+        active: !user.suspended,
+        bio: user?.customSchemas?.Custom_User_Fields.Bio,
+        bio_short: user?.customSchemas?.Custom_User_Fields.Bio_short,
+        expertise: user?.customSchemas?.Custom_User_Fields.Expertise,
+        Role: user?.customSchemas?.Custom_User_Fields.Role,
+        picture: user.thumbnailPhotoUrl,
+        organizations: user.organizations,
+        externalIds: user.externalIds,
+        emails: user.emails,
+        phones: user.phones
+      }));
     } catch (error) {
       console.error('Failed to get users:', error);
       throw error;
@@ -20,7 +40,7 @@ class GoogleDirectoryService {
       const res = await directory.groups.list({
         customer: 'my_customer',
         maxResults: 10,
-        orderBy: 'email',
+        orderBy: 'email'
       });
       return res.data.groups;
     } catch (error) {
